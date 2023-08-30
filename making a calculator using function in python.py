39620c4cf5b0cdc8a2ad25c67bce655dfd782970
Samim Osman Sabuj <86197373+samimosmansabuj@users.noreply.github.com>
2023-08-30 23:39:50 +0600

    Create making a calculator using function in python.py

---
+++ making a calculator using function in python.py
@@ -0,0 +1,19 @@
+_1st_number = float(input("Enter a First Number: "))
+select_operator = input("Enter Operator(+, -, *, / or %): ")
+_2nd_number = float(input("Enter a Second Number: "))
+def calculator(_1st_number, select_operator, _2nd_number):
+    if select_operator=="+":
+        equalation = _1st_number + _2nd_number
+    elif select_operator=="-":
+        equalation = _1st_number - _2nd_number
+    elif select_operator=="*":
+        equalation = _1st_number * _2nd_number
+    elif select_operator=="/":
+        equalation = _1st_number / _2nd_number
+    elif select_operator=="%":
+        equalation = _1st_number % _2nd_number
+    else:
+        equalation = "Wrong Input"
+    return equalation
+
+print(calculator(_1st_number, select_operator, _2nd_number))
