2fc10b1f718f4d1cc9081c727055923cb26c8039
Samim Osman Sabuj <86197373+samimosmansabuj@users.noreply.github.com>
2023-08-30 23:42:29 +0600

    Update making a calculator using function in python.py

--- making a calculator using function in python.py
+++ making a calculator using function in python.py
@@ -15,5 +15,5 @@ def calculator(_1st_number, select_operator, _2nd_number):
     else:
         equalation = "Wrong Input"
     return equalation
-
+#comment
 print(calculator(_1st_number, select_operator, _2nd_number))
